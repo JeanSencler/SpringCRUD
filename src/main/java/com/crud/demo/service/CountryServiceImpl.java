749430d7f855683d6c3564f9618e89f4626aa040
Jean Carlos Sencler <jeansencler@gmail.com>
2022-07-13 09:12:54 -0400

    [FIX] correcion metodo udpate

--- src/main/java/com/crud/demo/service/CountryServiceImpl.java
+++ src/main/java/com/crud/demo/service/CountryServiceImpl.java
@@ -35,7 +35,9 @@ public class CountryServiceImpl implements ICountryService {
 	public Country update(Country country, long countryId) throws CountryNotFoundException {
 		Country localCountry = this.getById(countryId);
 		if (!Objects.isNull(localCountry)) {
-			return countryRepository.save(country);
+			localCountry.setName(country.getName());
+			localCountry.setIndependent(country.getIndependent());
+			return countryRepository.save(localCountry);
 		} else {
 			throw new CountryNotFoundException(
 					CountryNotFoundException.COUNTRY_NOT_FOUND.concat(String.valueOf(countryId)));
