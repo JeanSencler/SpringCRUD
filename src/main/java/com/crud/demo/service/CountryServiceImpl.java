e000cd0d6ffb0524f5dcbbe64d4dd31af1448170
Jean Carlos Sencler <jeansencler@gmail.com>
2022-07-13 08:15:03 -0400

    [Change] ajustes de Mapeo de Response Externo y persistencia

--- src/main/java/com/crud/demo/service/CountryServiceImpl.java
+++ src/main/java/com/crud/demo/service/CountryServiceImpl.java
@@ -1,13 +1,19 @@
 package com.crud.demo.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import com.crud.demo.model.Country;
 import com.crud.demo.repo.ICountryRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class CountryServiceImpl implements ICountryService {
@@ -16,7 +22,7 @@ public class CountryServiceImpl implements ICountryService {
 	private ICountryRepository countryRepository;
 
 	@Override
-	public Country create(Country country) {
+	public Country create(Country country) throws CountryBadRequestException{
 		return countryRepository.save(country);
 	}
 
@@ -53,6 +59,19 @@ public class CountryServiceImpl implements ICountryService {
 			throw new CountryNotFoundException(
 					CountryNotFoundException.COUNTRY_NOT_FOUND.concat(String.valueOf(countryId)));
 		}
+	}
+
+	@Override
+	public List<?> getExternalCountryList(final String endpoint) throws RestClientException {
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity(endpoint, Object[].class);
+		Object[] objects = responseEntity.getBody();
+
+		ObjectMapper mapper = new ObjectMapper();
+		return Arrays.stream(objects).map(object -> mapper.convertValue(object, Country.class)).map(Country::getName)
+				.collect(Collectors.toList());
+
 	}
 
 }
